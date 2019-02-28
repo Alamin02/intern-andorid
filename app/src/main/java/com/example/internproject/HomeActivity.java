d4bba4b441d48815632a1d79f38858c49b1f933c
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-03-01 03:15:19 +0600

    Set up bottom nav

--- app/src/main/java/com/example/internproject/HomeActivity.java
+++ app/src/main/java/com/example/internproject/HomeActivity.java
@@ -1,7 +1,14 @@
 package com.example.internproject;
 
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,6 +17,27 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        BottomNavigationView menu = findViewById(R.id.navigationMenu);
 
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                TextView log2 = findViewById(R.id.logger);
+
+                switch (menuItem.getItemId()){
+                    case R.id.navigation_local:
+                        log2.setText("Local");
+                        return true;
+                    case R.id.navigation_server:
+                        log2.setText("Server");
+                        return true;
+                    case R.id.navigation_more:
+                        log2.setText("More");
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
