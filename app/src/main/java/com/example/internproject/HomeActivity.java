6d8cac538e4d16c9b5a36cac6916d035c02518b4
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-03-01 04:16:58 +0600

    Local Fragment modified

--- app/src/main/java/com/example/internproject/HomeActivity.java
+++ app/src/main/java/com/example/internproject/HomeActivity.java
@@ -3,6 +3,8 @@ package com.example.internproject;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,9 +26,12 @@ public class HomeActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 TextView log2 = findViewById(R.id.logger);
 
+                Fragment newFragment;
+
                 switch (menuItem.getItemId()){
                     case R.id.navigation_local:
-                        log2.setText("Local");
+                        newFragment = new LocalFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, newFragment).commit();
                         return true;
                     case R.id.navigation_server:
                         log2.setText("Server");
