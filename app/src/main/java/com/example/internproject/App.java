5a97cd7fc8dd79992bbdb91c0bd7ed3ba8e68d87
Alamin Hossain <alamin.ece.ku@gmail.com>
2019-03-04 19:01:46 +0600

    Fetch json

--- app/src/main/java/com/example/internproject/App.java
+++ app/src/main/java/com/example/internproject/App.java
@@ -5,10 +5,16 @@ import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseInstallation;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class App extends Application {
     public static ArrayList people = new ArrayList<Person>();
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -27,5 +33,24 @@ public class App extends Application {
         );
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+
+    }
+
+    public void get_json(){
+        String json;
+        try {
+            InputStream is = getAssets().open("data.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
